7a054a3990599a5075d5c541a015aa3669efc236
Alexander Poslushnyak <fr4nk.horr1gan@gmail.com>
2021-02-07 18:14:36 +0200

    added tmp render method (old ver)

--- jk-wp-author-dashboard.php
+++ jk-wp-author-dashboard.php
@@ -3,6 +3,96 @@
 class jk_wp_author_dashboard
 {
 
+    /* TODO Replace */
+    public static function dashboard_render($user_id)
+    {
+
+        ?>
+
+        <div class="author-dashboard">
+
+            <div class="inner-wrapper">
+
+                <div class="dashboard-header">
+
+                    <div class="user-info-wrapper">
+
+                    </div>
+
+                    <div class="dashboard-tabs-wrapper">
+
+                        <div class="tab-item">
+
+                            <?php echo esc_html('My Posts'); ?>
+
+                        </div>
+
+                        <div class="tab-item">
+
+                            <?php echo esc_html('Liked Posts'); ?>
+
+                        </div>
+
+                        <div class="tab-item">
+
+                            <?php echo esc_html('Liked Posts'); ?>
+
+                        </div>
+
+                        <div class="tab-item">
+
+                            <?php echo esc_html('Viewed Posts'); ?>
+
+                        </div>
+
+                        <div class="tab-item">
+
+                            <?php echo esc_html('Bookmarks'); ?>
+
+                        </div>
+
+                        <div class="tab-item">
+
+                            <?php echo esc_html('Following'); ?>
+
+                        </div>
+
+                        <div class="tab-item">
+
+                            <?php echo esc_html('Followers'); ?>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="dashboard-body">
+
+                    <div class="body-wrapper">
+
+                        <div class="tabs-content-wrapper">
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="dashboard-footer">
+
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <?php
+
+    }
+
     public function init()
     {
 
